1489b649402584ea423304dc413d49a50f9a2b3d
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-23 16:38:12 +0530

    User Registration User Vallidation Using Exception and Lambda Expression

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,34 +1,68 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+@FunctionalInterface
+interface valllidInterface
+{
+	public boolean vallidate(String pattern,String Input);
+}
 public class UserRegistration 
 {
-	public boolean vallidateName(String s)
+	static final String namePattern = "^[A-Z][a-z]{3,30}$";
+	static final String emailPattern = "^[0-9a-zA-Z]+ *([.+-_]?[a-zA-Z0-9]+)"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z ]{2,4}([.][a-zA-Z]{2,3})?$";
+	static final String phonePattern = "^[0-9]{2}\\s[0-9]{10}";
+	static final String passwordPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])(?=.{8,}).*$";
+	static valllidInterface vallidater=(pattern,input)-> Pattern.compile(pattern).matcher(input).matches(); 
+	public boolean vallidateName(String name) throws UserRegistrationException
 	{
-		String regex1="^([A-Z][a-z]{3,30})$";
-        Pattern p=Pattern.compile(regex1);
-        return p.matcher(s).matches();
+		if(vallidater.vallidate(namePattern,name))
+		{
+			System.out.println(name+"is vallid Name");
+			return true;
+		}
+		else
+		{
+			throw new UserRegistrationException(name+"is not vallid Name.Enter vallid Name");
+		}
 	}
 
-	public boolean vallidatePhoneNo(String PhoneNo) {
-		String regex1="^([0-9]{2}//s[0-9]{10})$";
-		Pattern p=Pattern.compile(regex1);
-		Matcher m1=p.matcher(PhoneNo);
-		return m1.matches();
-		
+	public boolean vallidatePhoneNo(String PhoneNo) throws UserRegistrationException 
+	{
+		if(vallidater.vallidate(phonePattern,PhoneNo))
+		{
+			System.out.println(PhoneNo+"is vallid Phone Number");
+			return true;
+		}
+		else
+		{
+			throw new UserRegistrationException(PhoneNo+"is not vallid Phone Number.Enter vallid Phone Number");
+		}
+
 	}
 
-	public boolean vallidatePassword(String passWord)
+	public boolean vallidatePassword(String passWord)throws UserRegistrationException 
 	{
-		String regex="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])(?=.{8,}).*$";
-        Pattern p=Pattern.compile(regex);
-        return p.matcher(passWord).matches();
+		if(vallidater.vallidate(passwordPattern,passWord))
+		{
+			System.out.println(passWord +"is vallid Password");
+			return true;
+		}
+		else
+		{
+			throw new UserRegistrationException(passWord+"is not vallid password.Enter Vallid Password");
+		}
 	}
-	public static boolean vallidateEmailId(String emailId)
+	public static boolean vallidateEmailId(String emailId)throws UserRegistrationException 
 	{
-		 String regex="^[0-9a-zA-Z]+ *([.+-_]?[a-zA-Z0-9]+)"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z ]{2,4}([.][a-zA-Z]{2,3})?$";
-	        Pattern p=Pattern.compile(regex);
-	       return p.matcher(emailId).matches();
+		if(vallidater.vallidate(emailPattern,emailId))
+		{
+			System.out.println(emailId +"is vallid Email-id");
+			return true;
+		}
+		else
+		{
+			throw new UserRegistrationException(emailId+"is not vallid Email-id.Enter Vallid Email-id");
+		}
+
 	}
 
 
